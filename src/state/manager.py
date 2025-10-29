309937ed8aef164e5c7dbf12051602152866fae3
al3nka <kiychenkoak@gmail.com>
2025-10-29 13:04:09 +0100

    Only overwrite device state and brightness if provided

--- src/state/manager.py
+++ src/state/manager.py
@@ -10,7 +10,7 @@ class StateManager:
     def __init__(self):
         self.session: Session = SessionLocal()
 
-    def save_device(self, type: DeviceType, pin: int, state: DeviceState = DeviceState.OFF, brightness: float = None):
+    def save_device(self, type: DeviceType, pin: int, state: DeviceState = None, brightness: float = None):
         device_id = f"{type}_{pin}"
         device = self.session.get(Device, device_id)
 
@@ -18,8 +18,10 @@ class StateManager:
             device = Device(type=type, pin=pin, state=state, brightness=brightness)
             self.session.add(device)
         else:
-            device.state = state
-            device.brightness = brightness
+            if state:
+                device.state = state
+            if brightness:
+                device.brightness = brightness
             device.last_updated = datetime.now()
 
         self.session.commit()
