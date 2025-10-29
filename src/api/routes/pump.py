1e2182846552562fb6145c0442eea7c30c6ad252
al3nka <kiychenkoak@gmail.com>
2025-10-29 13:05:40 +0100

    Remoce duplicaiton from route

--- src/api/routes/pump.py
+++ src/api/routes/pump.py
@@ -6,17 +6,17 @@ from constants import DeviceType, DeviceState
 pump_router = APIRouter(prefix="/pump", tags=["pump"])
 
 
-@pump_router.post("/pump/{pin}/start")
+@pump_router.post("/{pin}/start")
 def start_pump(pin: int):
     state_manager.save_device(type=DeviceType.PUMP, pin=pin, state=DeviceState.ON)
 
 
-@pump_router.post("/pump/{pin}/stop")
+@pump_router.post("/{pin}/stop")
 def stop_pump(pin: int):
     state_manager.save_device(type=DeviceType.PUMP, pin=pin, state=DeviceState.OFF)
 
 
-@pump_router.get("/pump/{pin}")
+@pump_router.get("/{pin}")
 def pin_status(pin: int):
     pin_state_values = state_manager.load_device(type=DeviceType.PUMP, pin=pin)
     return {"state": pin_state_values.state}
